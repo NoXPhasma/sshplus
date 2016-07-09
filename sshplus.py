f77a499313e6494b258ed0322516fd39c051916b
NoXPhasma <NoXPhasma@users.noreply.github.com>
2016-07-09 06:44:22 +0200

    Create and open ~/.sshplus if not present

--- sshplus.py
+++ sshplus.py
@@ -40,7 +40,6 @@ import shlex
 import re
 
 
-
 _VERSION = "1.0"
 
 _SETTINGS_FILE = os.getenv("HOME") + "/.sshplus"
@@ -77,15 +76,44 @@ Incorporating changes from simplestarter, Benjamin Heil, http://www.bheil.net
 
 Released under GPL3, http://www.gnu.org/licenses/gpl-3.0.html"""
 
+_EDIT_CONFIG = """To add items to the menu, edit the file <i>.sshplus</i> in your home directory. Each entry must be on a new line in this format:
+
+<tt>NAME|COMMAND|ARGS</tt>
+
+If the item is clicked in the menu, COMMAND with arguments ARGS will be executed. ARGS can be empty. To insert a separator, add a line which only contains "sep". Lines starting with "#" will be ignored. You can set an unclickable label with the prefix "label:". To insert a nested menu, use the prefix "folder:menu name". Subsequent items will be inserted in this menu, until a line containing an empty folder name is found: "folder:". After that, subsequent items get inserted in the parent menu. That means that more than one level of nested menus can be created.
+
+Example file:
+<tt><small>
+Show top|gnome-terminal|-x top
+sep
+
+# this is a comment
+label:SSH connections
+# create a folder named "Home"
+folder:Home
+SSH Ex|gnome-terminal|-x ssh user@1.2.3.4
+# to mark the end of items inside "Home", specify and empty folder:
+folder:
+# this item appears in the main menu
+SSH Ex|gnome-terminal|-x ssh user@1.2.3.4
+
+label:RDP connections
+RDP Ex|rdesktop|-T "RDP-Server" -r sound:local 1.2.3.4
+
+</small></tt>"""
+
+
 def menuitem_response(w, item):
     if item == '_about':
         show_help_dlg(_ABOUT_TXT)
+    elif item == '_edit':
+        edit_config_file()
     elif item == '_refresh':
         newmenu = build_menu()
         ind.set_menu(newmenu)
         notify2.init("sshplus")
-        notify2.Notification("SSHplus refreshed", "Config file \"%s\" has been read. Menu list was refreshed!" % _SETTINGS_FILE).show()
-    elif item == '_quit':
+        notify2.Notification("SSHplus aktualisiert", "\"%s\" wurde eingelesen! Menüliste wurde aktualisiert!" % _SETTINGS_FILE).show()
+    elif item == '_quit': 
         sys.exit(0)
     elif item == 'folder':
         pass
@@ -97,6 +125,8 @@ def menuitem_response(w, item):
 def show_help_dlg(msg, error=False):
     if error:
         dlg_icon = Gtk.MessageType.ERROR
+        md = Gtk.MessageDialog(None, 0, dlg_icon, Gtk.ButtonsType.OK, "This is an INFO MessageDialog")
+        edit_config_file()
     else:
         dlg_icon = Gtk.MessageType.INFO
         md = Gtk.MessageDialog(None, 0, dlg_icon, Gtk.ButtonsType.OK, "This is an INFO MessageDialog")
@@ -106,7 +136,15 @@ def show_help_dlg(msg, error=False):
         md.run()
     finally:
         md.destroy()
-    
+
+def edit_config_file():
+    if os.path.isfile(_SETTINGS_FILE) != True:
+        os.mknod(_SETTINGS_FILE)
+        show_help_dlg("<b>No <i>.sshplus</i> config file found, we created one for you!\n\nPlease edit the file and reload the config.</b>\n\n%s" % \
+             _EDIT_CONFIG, error=True)
+    os.spawnvp(os.P_NOWAIT, 'xdg-open', ['xdg-open', _SETTINGS_FILE])
+    os.wait3(os.WNOHANG)
+
 def add_separator(menu):
     separator = Gtk.SeparatorMenuItem()
     separator.show()
@@ -162,8 +200,8 @@ def get_sshplusconfig():
     return app_list
 
 def build_menu():
-    if not os.path.exists(_SETTINGS_FILE) and not os.path.exists(_SSHMENU_FILE) :
-        show_help_dlg("<b>ERROR: No .sshmenu or .sshplus file found in home directory</b>\n\n%s" % \
+    if not os.path.exists(_SETTINGS_FILE) :
+        show_help_dlg("<b>ERROR: No .sshmenu file found in home directory</b>\n\n%s" % \
              _ABOUT_TXT, error=True)
         sys.exit(1)
 
@@ -189,12 +227,12 @@ def build_menu():
 
     # Add SSHplus options folder to the end of the Menu
     add_separator(menu)
-    menu_item = add_menu_item(menus[-1], 'SSHplus Optionen', 'folder')
+    menu_item = add_menu_item(menus[-1], 'SSHplus Options', 'folder')
     menus.append(Gtk.Menu())
     menu_item.set_submenu(menus[-1])
-    add_menu_item(menus[-1], "Optionen", None)
-    add_menu_item(menus[-1], 'Konfigurieren', {'cmd': 'xdg-open', 'args': [os.getenv("HOME") + "/.sshplus"], 'name': 'Konfigurieren'})
-    add_menu_item(menus[-1], 'Aktualisieren', '_refresh')
+    add_menu_item(menus[-1], "Options", None)
+    add_menu_item(menus[-1], 'Edit', '_edit')
+    add_menu_item(menus[-1], 'Refresh', '_refresh')
     add_menu_item(menus[-1], 'About', '_about')
     add_separator(menus[-1])
     add_menu_item(menus[-1], 'Quit', '_quit')
@@ -207,6 +245,9 @@ if __name__ == "__main__":
 
     ind.set_label("Launch", "none")
     ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+    
+    if not os.path.exists(_SETTINGS_FILE) :
+        edit_config_file()
 
     appmenu = build_menu()
     ind.set_menu(appmenu)
